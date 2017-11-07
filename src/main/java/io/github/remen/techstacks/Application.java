8cad630d0bd11ee80921cd1b4a7459e2c0e1c161
Petter Remen <petter.remen@meltwater.com>
2017-11-07 08:52:19 +0100

    Add /health endpoint

--- src/main/java/io/github/remen/techstacks/Application.java
+++ src/main/java/io/github/remen/techstacks/Application.java
@@ -1,7 +1,39 @@
 package io.github.remen.techstacks;
 
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+
+import java.util.concurrent.CompletableFuture;
+
 public class Application {
+    private final Vertx vertx = Vertx.vertx();
+    private HttpServer server = vertx.createHttpServer();
+
     public static void main(String[] args) {
         System.out.println("Hello World");
+    }
+
+    public void start() {
+        CompletableFuture<Void> isListening = new CompletableFuture<>();
+        server
+            .requestHandler(request ->
+                request.response().setStatusCode(200).end()
+            )
+            .listen(8080, onListen -> {
+                if (onListen.succeeded()) {
+                    isListening.complete(null);
+                } else {
+                    isListening.completeExceptionally(onListen.cause());
+                }
+            });
+        isListening.join();
+    }
+
+    public void stop() {
+        server.close();
+    }
+
+    public int getPort() {
+        return 8080;
     }
 }
--- src/test/java/io/github/remen/techstacks/ApplicationTest.java
+++ src/test/java/io/github/remen/techstacks/ApplicationTest.java
@@ -1,13 +1,43 @@
 package io.github.remen.techstacks;
 
-import org.junit.jupiter.api.Assertions;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
-public class ApplicationTest {
-    @Test
-    @DisplayName("This test will intentionally fail")
-    void fail() {
-        Assertions.fail("Failing intentionally");
+import static org.assertj.core.api.Assertions.assertThat;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ApplicationTest {
+    private Application application = new Application();
+
+    @BeforeAll
+    void beforeAll() {
+        application.start();
+    }
+
+    @AfterAll
+    void afterAll() {
+        application.stop();
+    }
+
+    @Nested
+    @DisplayName("GET /health")
+    class get_health {
+        @Test
+        void responds_with_200() throws Exception {
+            OkHttpClient httpClient = new OkHttpClient.Builder().build();
+
+            Request request = new Request.Builder().url("http://localhost:" + application.getPort() + "/health").get().build();
+            try(Response response = httpClient.newCall(request).execute()) {
+                assertThat(response.code()).isEqualTo(200);
+            }
+        }
     }
 }
